47860ced81cd5d6a4fb7236ee095169b320e60da
jgwng <gunwng123@gmail.com>
2024-04-29 21:24:09 +0900

    Update : ãPC로 접근시에 뒤로/앞으로가기 버튼 이벤트 막기

--- lib/core/global_controller.dart
+++ lib/core/global_controller.dart
@@ -1,6 +1,7 @@
 
 import 'package:dohwaji/core/route_observer.dart';
 import 'package:dohwaji/core/routes.dart';
+import 'package:dohwaji/util/platform_util.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'dart:html' as html;
@@ -17,23 +18,42 @@ class GlobalController extends GetxController{
 
   @override
   void onInit(){
-   html.window.onPopState.listen((PopStateEvent event) {
-     var hash = window.location.hash;
-     if(ColorRouteObserver().hashList.contains(hash)){
-       try{
-         if (navigatorKey.currentState?.canPop() ?? false) {
-           navigatorKey.currentState?.pop();
-         } else {
-           debugPrint("Attempted to pop when no routes could be popped.");
-         }
-
-       }catch(e){
-         print(e.toString());
-       }
-     }else{
-       window.history.replaceState(null, '도화지', ColorRouteObserver().lastPath);
-     }
-    });
+    initWebSetting();
   }
+
+
+  void initWebSetting(){
+    if(PlatformUtil.isWeb == false) return;
+
+    if(PlatformUtil.isDesktopWeb){
+      html.window.history.pushState(null, '도화지', html.window.location.href);
+      html.window.onPopState.listen((event) {
+
+        html.window.history.go(1);
+      });
+    }else{
+      html.window.onPopState.listen((PopStateEvent event) {
+        var hash = window.location.hash;
+        if(ColorRouteObserver().hashList.contains(hash)){
+          try{
+            if (navigatorKey.currentState?.canPop() ?? false) {
+              navigatorKey.currentState?.pop();
+            } else {
+              debugPrint("Attempted to pop when no routes could be popped.");
+            }
+
+          }catch(e){
+            print(e.toString());
+          }
+        }else{
+          window.history.replaceState(null, '도화지', ColorRouteObserver().lastPath);
+        }
+      });
+    }
+
+  }
+
+
+
 
 }
